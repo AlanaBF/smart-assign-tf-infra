31b396db18c5ae1f19a465a37e183ff4ab42ff47
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2026-06-06 15:35:56 +0100

    error addressedd in tag

--- infra/_tags.tf
+++ infra/_tags.tf
@@ -5,6 +5,5 @@ locals {
     OwnerEmailAddress = var.tags["OwnerEmailAddress"]
     Project           = var.tags["Project"]
     Repository        = var.tags["Repository"]
-    Deployment-date   = formatdate("DD/MM/YYYY hh:mm:ss", timestamp())
   }, var.tags)
 }
